b3ea03539f69ba8089cd7765bec126a3d3aea91c
nilavya2000 <dnilavya@gmail.com>
2020-09-13 09:21:31 +0530

    updated

--- test.py
+++ test.py
@@ -2,8 +2,3 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-
-#import the data
-data=pd.read_csv('data/student.csv')
-print(data.head())
-print(data.isnull().sum()) #check if there is any null value or not
